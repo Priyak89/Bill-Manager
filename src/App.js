379919e66806cf06dbcbb828c6b6b38f64ddb99a
Priya <priya.kumaravelan@gmail.com>
2022-12-09 11:40:23 -0600

    REST API implementation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import BillManager from "./BillManager";
-
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
 function App() {
-  return <BillManager />;
+  return (
+    <div>
+      <Routes>
+        <Route exact path="/" element={<BillManager />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
